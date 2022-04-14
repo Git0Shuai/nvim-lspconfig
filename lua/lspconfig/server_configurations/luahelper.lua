df3c052f6dddf4cbc6e63a1cceab2b49066c216e
Shuai <heshuaix@qq.com>
2022-04-14 23:13:29 +0800

    add luahelper config

---
+++ lua/lspconfig/server_configurations/luahelper.lua
@@ -0,0 +1,28 @@
+local configs = require 'lspconfig.configs'
+local util = require 'lspconfig.util'
+
+-- Having server name defined here is the convention, this is often times also the first entry in the `cmd` table.
+local server_name = 'luahelper'
+
+configs[server_name] = {
+  default_config = {
+    -- This should be executable on the command line, arguments (such as `--stdio`) are additional entries in the list.
+    cmd = { 'luahelper' },
+    -- These are the filetypes that the server will either attach or start in response to opening. The user must have a filetype plugin matching the filetype, either via the built-in runtime files or installed via plugin.
+    filetypes = { 'lua' },
+    -- The root directory that lspconfig uses to determine if it should start a new language server, or attach the current buffer to a previously running language server.
+    root_dir = function(fname)
+        return util.root_pattern('luahelper.json')(fname) or util.find_git_ancestor(fname)
+    end,
+    single_file_support = true,
+    log_level = vim.lsp.protocol.MessageType.Warning,
+    settings = { 
+    },
+  },
+  docs = {
+    -- The description should include at minimum the link to the github project, and ideally the steps to install the language server.
+    description = [[
+tencent/luahelper
+]],
+  },
+}
